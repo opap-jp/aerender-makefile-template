cb8b64e348f4efd512a11c82fbabbe1add5bdf35
butameron <butameron@opap.jp>
2014-05-06 04:26:43 +0900

    add run-make.bat to make easy to execute make

---
+++ doga/run-make.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+
+SET MSYS_DIR=C:\MinGW\msys
+
+REM MSYS標準ではprofileで「cd $HOME」をしているので、作業ディレクトリが変わってしまう。
+REM それを回避する為、profileを読み込んでいない状態で一旦作業ディレクトリのパスを取得する。
+
+FOR /F "usebackq" %%w IN (`%MSYS_DIR%\bin\sh.exe -c pwd`) DO SET CUR_DIR=%%w
+
+
+REM 上記で取得した作業ディレクトリに移動してからmakeを実行する。
+
+%MSYS_DIR%\bin\sh.exe --login -i -c "cd %CUR_DIR% && make"
+
+PAUSE
